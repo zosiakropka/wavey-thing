e7ca94819daf4c82a4505cf23e0707156548262e
Szymon Maj <smaj@rocketlawyer.co.uk>
2017-01-20 20:28:41 +0000

    require and quintus added

---
+++ www/js/game.js
@@ -0,0 +1,9 @@
+define(['quintus-all'], function(){
+  var Q = Quintus()                          // Create a new engine instance
+    .include("Sprites, Scenes, Input, 2D, Touch, UI") // Load any needed modules
+    .setup()                           // Add a canvas element onto the page
+    .controls()                        // Add in default controls (keyboard, buttons)
+    .touch();                          // Add in touch support (for the UI)
+
+  return Q;
+})
---
+++ www/js/sprites/player.js
@@ -0,0 +1,33 @@
+define(['game'], function(Q){
+  Q.Sprite.extend("Player", {
+
+    init: function(p) {
+
+      // You can call the parent's constructor with this._super(..)
+      this._super(p, {
+        sheet: "player",  // Setting a sprite sheet sets sprite width and height
+        x: 410,           // You can also set additional properties that can
+        y: 90            // be overridden on object creation
+      });
+
+      // Add in pre-made components to get up and running quickly
+      this.add('2d, platformerControls');
+
+      // Write event handlers to respond hook into behaviors.
+      // hit.sprite is called everytime the player collides with a sprite
+      this.on("hit.sprite",function(collision) {
+        // Check the collision, if it's the Tower, you win!
+        if(collision.obj.isA("Tower")) {
+          // Stage the endGame scene above the current stage
+          Q.stageScene("endGame",1, { label: "You Won!" });
+          // Remove the player to prevent them from moving
+          this.destroy();
+        }
+      });
+    }
+
+  });
+
+  console.log(Q);
+
+});
