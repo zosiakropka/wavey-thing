5a57c164fce14c2f71ad523bc0e4ecf3f5e7bf0a
Zofia Wisniowolska <zofia.wisniowolska.ctr@sabre.com>
2017-01-21 00:56:20 +0100

    Position the floor on the screen

--- www/js/game.js
+++ www/js/game.js
@@ -13,7 +13,10 @@ define([
       Q.load(['peace.jpg', 'background-floor.png', 'background-wall.png'],
         function() {
           console.log('loaded some assets!');
-          Q.sheet('peace', 'peace.jpg', { tilew: 32, tileh: 32 });
+          Q.sheet('peace', 'peace.jpg', {
+            tilew: 200,
+            tileh: 300
+          });
           // Q.compileSheets("sprites.png","sprites.json");
           Q.stageScene("main");
         });
--- www/js/scenes/main.js
+++ www/js/scenes/main.js
@@ -1,10 +1,16 @@
 define([
     'game',
-    'sprites/player'
+    'sprites/player',
+    'jquery'
   ], function(
     Q,
-    Player
+    Player,
+    $
   ) {
+    var SCREEN_HEIGHT = $(window).height();
+    var SCREEN_MEEDLE_Y = SCREEN_HEIGHT / 2
+    var FLOOR_Y = SCREEN_HEIGHT * 0.8;
+
     Q.scene('main', function(stage){
       stage.insert(new Q.Repeater({
         asset: 'background-wall.png',
@@ -14,13 +20,18 @@ define([
       stage.insert(new Q.Repeater({
         asset: 'background-floor.png',
         speedX: 1,
-        y: 600
+        y: FLOOR_Y
       }));
 
       var player = stage.insert(new Player({
         x: 10,
-        y: 20
+        y: 0
       }));
-      stage.add('viewport').follow(player);
+
+      stage
+        .add('viewport')
+        .follow(player);
+
+      stage.viewport.offsetY = SCREEN_MEEDLE_Y - SCREEN_HEIGHT + FLOOR_Y;
     });
   });
