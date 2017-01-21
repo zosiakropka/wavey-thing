4ac8fcb54eda05cb91d32cf1af2a1f7c25dca5d8
Zofia Wisniowolska <zofia.wisniowolska.ctr@sabre.com>
2017-01-21 23:05:17 +0100

    remove stuff from index.js

--- www/js/index.js
+++ www/js/index.js
@@ -27,15 +27,8 @@ requirejs([
       },
 
       // Update DOM on a Received Event
-      receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+      receivedEvent: function() {
+        console.info('deviceready');
       }
     };
 
