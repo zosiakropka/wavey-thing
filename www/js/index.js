bc16636bfc91fcd1bcf4ffff39e57c674d3fb6fb
Zofia Wisniowolska <zofia.wisniowolska.ctr@sabre.com>
2017-01-21 21:26:10 +0100

    Move bower components to www

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,9 @@
 requirejs.config({
   paths: {
-    'components': '../../bower_components',
-    'underscore': '../../bower_components/underscore/underscore',
-    'jquery': '../../bower_components/jquery/dist/jquery',
-    'require-promise': '../../bower_components/requirejs-promise/requirejs-promise'
+    'components': '../bower_components',
+    'underscore': '../bower_components/underscore/underscore',
+    'jquery': '../bower_components/jquery/dist/jquery',
+    'require-promise': '../bower_components/requirejs-promise/requirejs-promise'
   }
 });
 
