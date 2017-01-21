be1604f4b8aaa70b5047a500730bb5f274012b00
Szymon Maj <smaj@rocketlawyer.co.uk>
2017-01-22 00:06:05 +0100

    cordova config

--- www/js/index.js
+++ www/js/index.js
@@ -12,27 +12,5 @@ requirejs([
   ], function(
     GamePlay
   ) {
-    var app = {
-      // Application Constructor
-      initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-      },
-
-      // deviceready Event Handler
-      //
-      // Bind any cordova events here. Common events are:
-      // 'pause', 'resume', etc.
-      onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-      },
-
-      // Update DOM on a Received Event
-      receivedEvent: function() {
-        console.info('deviceready');
-      }
-    };
-
-    app.initialize();
-
     GamePlay.startGame();
   });
