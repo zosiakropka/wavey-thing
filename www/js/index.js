b620e7f8cd61a92626f79047e205a3a19c61052b
Zofia Wisniowolska <zofia.wisniowolska.ctr@sabre.com>
2017-01-22 08:34:59 +0100

    Move the buttons on the screen

--- www/js/index.js
+++ www/js/index.js
@@ -8,9 +8,42 @@ requirejs.config({
 });
 
 requirejs([
+    'jquery',
     'gameplay'
   ], function(
+    $,
     GamePlay
   ) {
+    _scaleToFit();
     GamePlay.startGame();
+
+    function _scaleToFit() {
+      var GAME_WIDTH = 1920;
+      var GAME_HEIGHT = 1080;
+      var GAME_RATIO = GAME_WIDTH / GAME_HEIGHT;
+      var width = $(window).width();
+      var height = $(window).height();
+      var ratio = width / height;
+
+      var initialScale;
+
+      if (ratio > GAME_RATIO) {
+        initialScale = GAME_HEIGHT / height;
+      } else {
+        initialScale = GAME_WIDTH / width;
+      }
+
+      var viewport = $('meta[name="viewport"]');
+      var settings = [
+        'user-scalable=no, initial-scale=',
+        initialScale,
+        ', maximum-scale=',
+        initialScale,
+        ', minimum-scale=',
+        initialScale,
+        ', width=',
+        GAME_WIDTH
+      ].join('');
+      viewport.prop('content', settings)
+    }
   });
